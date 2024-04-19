5ea35023092ca116d9400a20882ba723330de183
Hardik Chavda <hardik.chavda@dakshyafreondtech.com>
2024-04-19 11:13:44 +0530

    added string creation

---
+++ string.js
@@ -0,0 +1,5 @@
+let message = "Hello";
+console.log(typeof message); // create string with literal.
+
+let msg = new String("Hello string constructor");
+console.log(typeof msg); // create string with String constructor;
