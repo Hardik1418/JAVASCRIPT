d3a9e80922fa99ef66bc313f6095b8f706d5a565
Hrdk1409 <hrdktech1409@gmail.com>
2024-05-01 12:26:07 +0530

    added basic array and its function

---
+++ PRACTICE/arrays.js
@@ -0,0 +1,18 @@
+let array = [1,2,1,3,4];
+
+//console.log(array.length);
+console.log(array);
+
+// array.forEach(function(items){
+//     items++;
+// });s
+
+//array.push(100);   //to add the element at the last
+//array.pop();       // To remove the last element from the array.
+//array.shift();     // To remove the element from the beginning of the array.
+//array.unshift(100);  // To add the element at the front side of the array.
+//let pos = array.indexOf(2);
+//console.log(pos);
+array.slice();
+
+console.log(array);
