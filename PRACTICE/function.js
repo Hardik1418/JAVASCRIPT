8319a094fdb88dac91455fea1aa3a9add4d3c66f
Hrdk1409 <hrdktech1409@gmail.com>
2024-05-11 14:53:58 +0530

    DOM

---
+++ PRACTICE/function.js
@@ -0,0 +1,32 @@
+//Normal function 
+
+// function doSum(a, b) {
+//     let c = a + b;
+
+//     return c;
+// }
+
+// console.log("ADD TWO NUMBER")
+// let sum = doSum(7,3);
+// console.log("Addition: "+ sum);
+
+// Arrow Function 
+
+// const doMult = (a, b)=> {
+
+//     let c = a * b;
+//     return c;
+// };
+
+// let mul = doMult(5,5);
+// console.log(mul);
+
+//IIFE(Immediately Invoked function expression)
+
+// (function hardik() {
+//     console.log("Hello Hardik");
+// })();
+
+// ((name)=> {
+//     console.log(`hello ${name}....`);
+// })("Chavda Hardik");
